085ea45543891e8cf5dee4c12105969e4c253ac8
Павел Хрущёв <charvelcjc1113712@outlook.com>
2022-01-27 14:42:53 +0800

    some edit

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -9,11 +9,11 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Bridge is AccessControl, ERC721Holder {
     using ECDSA for bytes32;
-    enum status { EMPTY, SWAPED, REDEEMED }
     bytes32 public constant CHAIR_PERSON = keccak256("CHAIR_PERSON");
-    address public ownerNFT;
     uint256 public chainId;
+    address public ownerNFT;
     mapping (bytes32 => status) swaps;
+    enum status { EMPTY, SWAPED, REDEEMED }
     
 
     constructor(address _nftAddress, uint256 _chainId) {
@@ -25,7 +25,11 @@ contract Bridge is AccessControl, ERC721Holder {
 
     event swapInitialized(uint256 _itemId, address _owner, uint256 _chainIDfrom, uint256 _chainIDto);
 
-    function swap(uint256 _itemId, uint256 _chainIdTo, uint256 nonce) external {
+    function swap(
+        uint256 _itemId, 
+        uint256 _chainIdTo, 
+        uint256 nonce
+    ) external {
         require(NFT(ownerNFT).ownerOf(_itemId) == msg.sender, "User has no rights to this token");
         bytes32 dataHash = keccak256(
             abi.encodePacked(_itemId, msg.sender, nonce, chainId, _chainIdTo)
@@ -35,13 +39,21 @@ contract Bridge is AccessControl, ERC721Holder {
         emit swapInitialized(_itemId, msg.sender, chainId, _chainIdTo);
     }
 
-    function redeem(uint256 _itemId, address _owner, uint256 _chainIdFrom, uint256 _nonce,
-                uint8 _v, bytes32 _r, bytes32 _s) external onlyRole(CHAIR_PERSON){
+    function redeem(
+        uint256 _itemId,
+        address _owner, 
+        uint256 _chainIdFrom, 
+        uint256 _nonce,
+        uint8 _v, 
+        bytes32 _r, 
+        bytes32 _s
+    ) external onlyRole(CHAIR_PERSON){
         
         bytes32 dataHash = keccak256(
             abi.encodePacked(_itemId, _owner, _nonce, _chainIdFrom, chainId)
         );
-        require(ECDSA.recover(dataHash.toEthSignedMessageHash(), _v, _r, _s) == (msg.sender), "Signature is wrong");
+        address signer = ECDSA.recover(dataHash.toEthSignedMessageHash(), _v, _r, _s);
+        require(signer == msg.sender, "Signature is wrong");
         require(swaps[dataHash] != status.REDEEMED, "Already Redeemed");
         swaps[dataHash] = status.REDEEMED;
         NFT(ownerNFT).mint(_owner, _itemId);
