a2d54b0cac82bff4af698832fbcf43352076cc30
Павел Хрущёв <charvelcjc1113712@outlook.com>
2022-05-25 00:59:08 +0800

    update

--- contracts/IERC721.sol
+++ contracts/IERC721.sol
@@ -7,13 +7,11 @@ interface IERC721 is IERC165 {
         address indexed _to,
         uint256 indexed _tokenId
     );
-
     event Approval(
         address indexed _owner,
         address indexed _approved,
         uint256 indexed _tokenId
     );
-
     event ApprovalForAll(
         address indexed _owner,
         address indexed _operator,
@@ -23,6 +21,23 @@ interface IERC721 is IERC165 {
     function balanceOf(address _owner) external view returns (uint256);
 
     function ownerOf(uint256 _tokenId) external view returns (address);
+
+    function approve(address _approved, uint256 _tokenId) external payable;
+
+    function getApproved(uint256 _tokenId) external view returns (address);
+
+    function setApprovalForAll(address _operator, bool _approved) external;
+
+    function isApprovedForAll(address _owner, address _operator)
+        external
+        view
+        returns (bool);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
 
     function safeTransferFrom(
         address _from,
@@ -36,23 +51,6 @@ interface IERC721 is IERC165 {
         address _to,
         uint256 _tokenId
     ) external payable;
-
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _tokenId
-    ) external payable;
-
-    function approve(address _approved, uint256 _tokenId) external payable;
-
-    function setApprovalForAll(address _operator, bool _approved) external;
-
-    function getApproved(uint256 _tokenId) external view returns (address);
-
-    function isApprovedForAll(address _owner, address _operator)
-        external
-        view
-        returns (bool);
 }
 
 interface IERC165 {
